598dfc08a5bda356f2363443e49fd354a7c81acb
tomer gur <tomer.gur@amwell.com>
2026-08-02 16:38:07 +0300

    Board: default date to today, remove meeting objective field, root redirects to hub

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'redirect-root-to-hub',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/' || req.url === '/index.html') {
+            res.writeHead(302, { Location: '/hub.html' })
+            res.end()
+            return
+          }
+          next()
+        })
+      },
+    },
+  ],
   base: '/',
   server: {
     proxy: {
